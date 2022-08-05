fcedb53108d46e82fa6a9fadea55786a27e40d43
bishaldn <bdn0298@gmail.com>
2022-08-05 09:06:50 +0530

    nthing but a commit

--- js/rule.js
+++ js/rule.js
@@ -4,11 +4,7 @@ setTimeout(() => {
     start.src = './sound/track.mp3';
     start.play();
 }, 3000);
-setTimeout(() => {
-    const start = new Audio();
-    start.src = './sound/track.mp3';
-    start.play();
-}, 34000);
+
 
 
 
