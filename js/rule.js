2a51afee7854a4b4432825de34f7929daedc3520
bishaldn <bdn0298@gmail.com>
2022-08-05 08:55:01 +0530

    update 2.0

--- js/rule.js
+++ js/rule.js
@@ -1,19 +1,15 @@
-// setTimeout(() => {
-//     const start = new Audio();
-//     start.src = './sound/Marshal Bgm - Lokiuniverse Remix BGM.mp3';
-//     start.play();
-// }, 1);
 
 setTimeout(() => {
     const start = new Audio();
-    start.src = './sound/Harry Potter Trap - Remix - Instrumental - Bgm.mp3';
+    start.src = './sound/track.mp3';
     start.play();
-}, 1000);
+}, 3000);
 setTimeout(() => {
     const start = new Audio();
-    start.src = './sound/start-music.mp3';
+    start.src = './sound/track.mp3';
     start.play();
-}, 36000);
+}, 34000);
+
 
 
  
